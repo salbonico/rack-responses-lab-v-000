dded83b3522b5cabf5a06e633a25c08318f08686
Steve Albonico <salbonico@gmail.com>
2019-02-01 19:39:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
   def call(env)
 
       resp = Rack::Response.new
-
-      resp.write Time.local(Time.now)
+    
+      resp.write Time.local(Time.now.to_a)
 
 
       resp.finish
