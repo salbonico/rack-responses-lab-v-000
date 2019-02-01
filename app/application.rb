0109fe2f5f0af5d321230e23da9ef86d2be04f63
Steve Albonico <salbonico@gmail.com>
2019-02-01 19:33:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,9 @@ class Application
 
       resp = Rack::Response.new
       time1 = Time.new
+       time1 = PST
       resp.write time1.localtime
+
       resp.write time1.zone
       resp.finish
   end
