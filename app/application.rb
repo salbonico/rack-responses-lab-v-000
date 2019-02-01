3a5eef312efe2f05473bd8733b5e8c29eca153fe
Steve Albonico <salbonico@gmail.com>
2019-02-01 19:35:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,8 @@ class Application
   def call(env)
 
       resp = Rack::Response.new
-      time1 = Time.new
-       time1 = "PST"
-      resp.write time1
+
+      resp.write Time.now
 
       resp.write time1.zone
       resp.finish
